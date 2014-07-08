960c5d6fb3100b77927091e11bacd83e61fe8e7e
Zef Houssney <zef@madebykiwi.com>
2014-07-08 08:10:05 -0600

    Improve hydra configuration.

--- hydra/init.lua
+++ hydra/init.lua
@@ -1,7 +1,7 @@
 -- Hi!
 -- Save this as ~/.hydra/init.lua and choose Reload Config from the menu
 
-pathwatcher.new(os.getenv("HOME") .. "/.hydra/", hydra.reload):start()
+-- pathwatcher.new(os.getenv("HOME") .. "/.hydra/", hydra.reload):start()
 autolaunch.set(true)
 logger.lines = {}
 -- logger.show()
@@ -36,50 +36,128 @@ menu.show(function()
 end)
 
 -- utilities
+local function manualReload()
+  hydra.alert("Reloading Hydra...", 0.8)
+  hydra.reload()
+end
+hotkey.bind({"alt"}, "9", manualReload)
+
 local function log(string)
   print(string)
 end
 
-local function logFrame(frame)
-  print("x: " .. frame.x, ", Y: " .. frame.y, "\nW: " .. frame.w, ", H: " .. frame.h .. "\n")
+local function frameString(frame)
+  return "x: " .. frame.x .. " Y: " .. frame.y .. " @ " .. frame.w .. "x" .. frame.h
 end
 
-local function close(a, b)
-  local threshold = 5
-  return a == b or (a > b-5 and a < b+5)
+local function logFrame(frame, title)
+  if title then
+    print(title .. ": " .. frameString(frame))
+  else
+    print(frameString(frame))
+  end
+end
+
+
+
+local function isClose(a, b)
+  local threshold = 12
+  return a == b or (a > b - threshold and a < b + threshold)
 end
 
 local function framesEqual(a, b)
   -- x and y must be exact
-  -- width and height use close() because they can be imprecise depending on the application
-  return a.x == b.x and a.y == b.y and close(a.w, b.w) and close(a.h, b.h)
+  -- width and height use isClose() because they can be imprecise depending on the application
+  return a.x == b.x and a.y == b.y and isClose(a.w, b.w) and isClose(a.h, b.h)
 end
 
-namedFrames = {}
 frameGroups = {}
-
-local function generateNamedFrames()
+local function groupForScreen(group, screen)
   local win = window.focusedwindow()
-  local s = win:screen():frame_without_dock_or_menu()
+  local s = screen:frame_without_dock_or_menu()
+  local screenKey = s.x .. s.y .. s.w .. s.h
 
-  namedFrames.leftHalf = { x = s.x, y = s.y, w = s.w/2, h = s.h }
-  namedFrames.rightHalf = { x = s.w/2, y = s.y, w = s.w/2, h = s.h }
+  if frameGroups[screenKey] then
+    if frameGroups[screenKey][group] then
+      return frameGroups[screenKey][group]
+    end
+  else
+    frameGroups[screenKey] = {}
+  end
 
-  frameGroups.halves = { namedFrames.leftHalf, namedFrames.rightHalf }
+  -- hydra.alert(screenKey, 2)
+  local newGroup = {}
+
+  if group == "halves" then
+    newGroup = {
+      -- left half
+      geometry.rect(s.x, s.y, s.w/2, s.h),
+      -- right half
+      geometry.rect(s.x + s.w/2, s.y, s.w/2, s.h)
+    }
+  elseif group == "center" then
+    local centeredOffset = 0.2
+    newGroup = {
+      geometry.rect(s.x, s.y, s.w, s.h),
+      geometry.rect(s.x + s.w * centeredOffset, s.y, s.w - s.w * centeredOffset * 2, s.h),
+    }
+  elseif group == "quarters" then
+    local halfWidth = s.w/2
+    local halfHeight = s.h/2
+    newGroup = {
+      -- top left
+      geometry.rect(s.x, s.y, halfWidth, halfHeight),
+      -- top right
+      geometry.rect(s.x + s.w/2, s.y, halfWidth, halfHeight),
+      -- bottom right
+      geometry.rect(s.x + s.w/2, s.y + halfHeight, halfWidth, halfHeight),
+      -- bottom left
+      geometry.rect(s.x, s.y + halfHeight, halfWidth, halfHeight)
+    }
+  elseif group == "verticalThirds" then
+    local thirdHeight = math.ceil(s.h/3)
+    newGroup = {
+      -- bottom to top
+      geometry.rect(s.x, s.y + thirdHeight * 2, s.w, thirdHeight),
+      geometry.rect(s.x, s.y + thirdHeight, s.w, thirdHeight),
+      geometry.rect(s.x, s.y, s.w, thirdHeight)
+    }
+  elseif group == "horizontalThirds" then
+    local thirdWidth = math.ceil(s.w/3)
+    newGroup = {
+      geometry.rect(s.x, s.y, thirdWidth, s.h),
+      geometry.rect(s.x + thirdWidth, s.y, thirdWidth, s.h),
+      geometry.rect(s.x + thirdWidth * 2, s.y, thirdWidth, s.h),
+    }
+  else
+    hydra.alert("No group found: " .. group, 5)
+    return nil
+  end
+
+  frameGroups[screenKey][group] = newGroup
+  return newGroup
 end
 
-generateNamedFrames()
+local function framesForScreenGroup(screenId, group)
+  -- hydra.alert(frameGroups["halves"][2].x, 2)
+end
 
-local function halves()
+-- framesForScreenGroup()
+
+local function cyclePositionGroup(groupName)
+  -- hydra.alert(group[2].x, 2)
   local win = window.focusedwindow()
+  local group = groupForScreen(groupName, win:screen())
+
+  if (group == nil) then return end
+
   local currentFrame = win:frame()
-
-  group = frameGroups.halves
-
   local targetIndex = 1
   for index, frame in ipairs(group) do
+    -- logFrame(currentFrame, "current")
+    -- logFrame(frame, "testing against")
     if framesEqual(currentFrame, frame) then
-      log("Frame " .. index .. " is equal")
+      -- log("Frame " .. index .. " is equal")
       if index < #group then
         targetIndex = index + 1
       end
@@ -91,7 +169,49 @@ local function halves()
   win:setframe(targetFrame)
 end
 
-hotkey.bind({"alt"}, "1", halves)
+local function frameOnScreen(frame, screen)
+  return geometry.rect(
+    frame.x - screen.x,
+    frame.y - screen.y,
+    frame.w,
+    frame.h
+  )
+end
+
+local function nextScreen()
+  local win = window.focusedwindow()
+	local frame = win:frame()
+	local thisScreen = win:screen():frame_including_dock_and_menu()
+	local nextScreen = win:screen():next():frame_including_dock_and_menu()
+
+  local adjustedFrame = frameOnScreen(frame, thisScreen)
+
+  local widthFactor = nextScreen.w / thisScreen.w
+  local heightFactor = nextScreen.h / thisScreen.h
+
+  local newFrame = geometry.rect(
+    adjustedFrame.x * widthFactor + nextScreen.x,
+    adjustedFrame.y * heightFactor + nextScreen.y,
+    adjustedFrame.w * widthFactor,
+    adjustedFrame.h * heightFactor
+  )
+
+  -- logFrame(frame, "Frame")
+  -- logFrame(thisScreen, "This")
+  -- logFrame(nextScreen, "Next")
+  -- logFrame(newFrame, "New frame")
+  -- print("-----")
+
+	win:setframe(newFrame)
+end
+
+hotkey.bind({"alt"}, "0", nextScreen)
+
+hotkey.bind({"alt"}, "1", function() cyclePositionGroup("halves") end)
+hotkey.bind({"alt"}, "2", function() cyclePositionGroup("center") end)
+hotkey.bind({"alt"}, "3", function() cyclePositionGroup("quarters") end)
+hotkey.bind({"alt"}, "4", function() cyclePositionGroup("verticalThirds") end)
+hotkey.bind({"alt"}, "5", function() cyclePositionGroup("horizontalThirds") end)
 
 
 -- move the window to the right a bit, and make it a little shorter
