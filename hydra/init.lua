373fa43f7cfbec743927333d08b49a49eaaea2a0
Zef Houssney <zef@madebykiwi.com>
2014-08-12 11:05:40 -0600

    Remove hydra check for updates.

--- hydra/init.lua
+++ hydra/init.lua
@@ -14,31 +14,13 @@ logger.lines = {}
 --   almost all readline functionality works in the repl
 hotkey.bind({"cmd", "ctrl", "alt"}, "R", repl.open)
 
--- save the time when updates are checked
-function checkforupdates()
-  hydra.updates.check(function(available)
-      -- what to do when an update is checked
-      if available then
-        notify.show("Hydra update available", "", "Click here to see the changelog and maybe even install it", "showupdate")
-      else
-        hydra.alert("No update available.")
-      end
-  end)
-  hydra.settings.set('lastcheckedupdates', os.time())
-end
-
 
 -- show a helpful menu
 menu.show(function()
-    local updatetitles = {[true] = "Install Update", [false] = "Check for Update..."}
-    local updatefns = {[true] = updates.install, [false] = checkforupdates}
-    local hasupdate = (updates.newversion ~= nil)
-
     return {
       {title = "Reload Config", fn = hydra.reload},
       {title = "-"},
       {title = "About", fn = hydra.showabout},
-      {title = updatetitles[hasupdate], fn = updatefns[hasupdate]},
       {title = "Quit Hydra", fn = os.exit},
     }
 end)
@@ -254,32 +236,3 @@ hotkey.bind({"alt"}, "9", manualReload)
 --     win:setframe(frame)
 -- end):enable()
 
--- show available updates
-local function showupdate()
-  os.execute('open https://github.com/sdegutis/Hydra/releases')
-end
-
--- check for updates every week
-timer.new(timer.weeks(1), checkforupdates):start()
-notify.register("showupdate", showupdate)
-
--- if this is your first time running Hydra, you're launching it more than a week later, check now
-local lastcheckedupdates = settings.get('lastcheckedupdates')
-if lastcheckedupdates == nil or lastcheckedupdates <= os.time() - timer.days(7) then
-  checkforupdates()
-end
-
-
-
-
--- I've worked hard to make Hydra useful and easy to use. I've also
--- released it with a liberal open source license, so that you can do
--- with it as you please. So, instead of charging for licenses, I'm
--- asking for donations. If you find it helpful, I encourage you to
--- donate what you believe would have been a fair price for a license:
-
-local function donate()
-  os.execute("open 'https://www.paypal.com/cgi-bin/webscr?business=sbdegutis@gmail.com&cmd=_donations&item_name=Hydra.app%20donation'")
-end
-
-hotkey.bind({"cmd", "alt", "ctrl"}, "D", donate)
